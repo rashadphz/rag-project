998fd406334322597f80c0dbb51856d74037cd5e
Rashad Philizaire <rashadphilizaire@gmail.com>
2024-04-03 20:10:16 -0500

    added all jsons

--- parse.py
+++ parse.py
@@ -1,9 +1,6 @@
 import asyncio
 import nest_asyncio
-from llama_index.llms.openai import OpenAI
-from llama_index.core import Settings
 from llama_parse import LlamaParse
-from llama_index.core import SimpleDirectoryReader
 import os
 from dotenv import load_dotenv
 import json
